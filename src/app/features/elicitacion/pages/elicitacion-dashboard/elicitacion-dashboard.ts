e4eece7ae74d24d6d20b931baf26a12c60bb017c
LuisAndana <133995486+LuisAndana@users.noreply.github.com>
2026-02-11 23:19:36 -0700

    elicitacion
    diseño 1

--- src/app/features/elicitacion/pages/elicitacion-dashboard/elicitacion-dashboard.ts
+++ src/app/features/elicitacion/pages/elicitacion-dashboard/elicitacion-dashboard.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-elicitacion-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatDividerModule
+  ],
   templateUrl: './elicitacion-dashboard.html',
-  styleUrl: './elicitacion-dashboard.css',
+  styleUrls: ['./elicitacion-dashboard.css']
 })
-export class ElicitacionDashboard {
-
-}
+export class ElicitacionDashboard {}
